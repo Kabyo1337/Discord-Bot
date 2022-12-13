e0ea83a948dc31fad88e268d6731d662d6401739
Kabyo1337~ <siyamkabyo@gmail.com>
2022-12-13 06:44:35 +0600

    first commit

---
+++ bot.js
@@ -0,0 +1,33 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require(`discord.js`);
+
+const prefix = '>';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready" , () => {
+    console.log("bot is online")
+
+    client.user.setActivity(`fakerdev` , { type: "WATCHING" });
+})
+
+client.on("messageCreate", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commnad = args.shift().toLowerCase();
+
+
+    const messageArray = message.content.split(" ");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+
+    if (commnad === 'hi') {
+        message.channel.send("Know thy neighbour")
+    }
+
+})
+
+
+
+client.login("MTA1MjAwMjY2MzgyNDM2MzYwMQ.G8jcNR.OVU4tlhOTZtrlN5VKrKTVKj8PMalVeEDXr-TVI");
